54593105df871668a42d9b41a2d8ddb5c0d135bd
berryberry189 <berryberry189@naver.com>
2022-10-03 21:51:33 +0900

    feat: 동시성 문제점 발생시 문제점 테스트 케이스 작성

--- src/test/java/com/grace/stock/domain/stock/service/StockServiceTest.java
+++ src/test/java/com/grace/stock/domain/stock/service/StockServiceTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.grace.stock.domain.stock.Stock;
 import com.grace.stock.domain.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +22,7 @@ class StockServiceTest {
   private StockRepository stockRepository;
 
   // 데이터 세팅
+
   @BeforeEach
   void before() {
     Stock stock = new Stock(1L, 100);
@@ -41,4 +45,33 @@ class StockServiceTest {
 
     assertEquals(99, stock.getQuantity());
   }
+
+  @Test
+  void 동시에_100개의_요청() throws InterruptedException {
+    int threadCount = 100;
+    // 멀티 스레드 생성
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+    // 다른 스레드에서 수행중인 작업이 완료될때 까지 대기할 수 있도록 도와주는 class
+    CountDownLatch latch = new CountDownLatch(threadCount);
+
+    for(int i=0; i <threadCount; i++) {
+      executorService.submit(() -> {
+        try {
+          stockService.decrease(1L, 1);
+        } finally {
+          latch.countDown();
+        }
+      });
+    }
+
+    latch.await();
+
+    Stock stock = stockRepository.findById(1L).orElseThrow();
+
+    // 테스트 실패됨
+    assertEquals(0, stock.getQuantity());
+  }
+
+
 }
